7302de749c14eaf6d11cde001a4d762de18c9097
sreekar <sreekar@gmail.com>
2024-11-18 09:23:58 +0530

    rct

---
+++ r2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Good Morning  = () => {
+  return <h1>Good, Morning!</h1>;
+};
+
+export default Good Morning;
---
+++ r3.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Getoutall = () => {
+  return <h1>Getout, all!</h1>;
+};
+
+export default Getoutall;
